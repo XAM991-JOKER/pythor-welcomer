a58ab4b2c72497066da5b07f7a066d0bc0ee71a0
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-09-14 19:16:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,4 +159,24 @@ client.on("ready", () => {
  
 
 
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' WELCOME ','FINEX SERVER | FX ','BY : بايثور ','HAY [=',' Hi (='];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`https://www.twitch.tv/pythorxxx`);
+    }, ms);
+
+})
+
+
+
 client.login(process.env.BOT_TOKEN);
